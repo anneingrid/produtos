997cfa6f3e519f4f236297f5c0b4a4fd51f72748
Anne Ingrid <103117810+anneingrid@users.noreply.github.com>
2022-12-05 17:57:20 -0300

    Update anne.py

--- anne.py
+++ anne.py
@@ -1,13 +1,14 @@
-produtos=open('produtos.txt','+r')
 def trataArquivo(arq):
-    conteudo=arq.read().splitlines()
+    file=open(arq,'+r')
+    conteudo=file.read().splitlines()
+    file.close()
     nova=[]
     for linha in conteudo:
         if ';' in linha:
             linha=linha.replace(';','')
         nova.append(linha.split('-'))
+        
     return(nova)
-
 
 
 def maiorValor(arq):
@@ -35,20 +36,19 @@ def somaTotal(arq):
     for item in lista:
         item[1]=float(item[1].replace(',','.'))        
         soma+=item[1]
-    return(soma)
+    return round(soma,2)
 
 opcao = 0
 while opcao!=4:
     if opcao == 1:
-       print("O produto de maior valor ", maiorValor(produtos))
+       print("O produto de maior valor ", maiorValor('produtos.txt'))
     elif opcao == 2:
-        print("O produto de menor valor ", menorValor(produtos))
+        print("O produto de menor valor ", menorValor('produtos.txt'))
     elif opcao == 3:
-      print("valor total dos produtos ", somaTotal(produtos))
+      print("valor total dos produtos ", somaTotal('produtos.txt'))
     opcao = int(input("\n Escolha a opção desejada:"\
               "\n1. Encontrar o maior valor."\
               "\n2. Encontrar o menor valor."\
               "\n3. Total"\
               "\n4. Sair\n"))
 
-    arq.close()
